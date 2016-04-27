3e7fab58ad215f763df8b0bd0fc8db5f7d7f93ad
Joaquin <jkinmozas@gmail.com>
2016-04-27 23:19:07 +0200

    Creada la clase Proveedor con sus atributos, getters y setters y constructores necesarios

---
+++ src/Modelo/Proveedor.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Joaquin
+ */
+public class Proveedor {
+
+    private String NIF;
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private String email;
+
+    public Proveedor(String NIF, String nombre, String direccion, int telefono, String email) {
+        this.NIF = NIF;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNIF() {
+        return NIF;
+    }
+
+    public void setNIF(String NIF) {
+        this.NIF = NIF;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
