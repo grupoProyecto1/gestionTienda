edb2ea4d674e88ed785aea47f58c28cd76ecd392
Joaquin <jkinmozas@gmail.com>
2016-04-28 00:39:19 +0200

    Creada la clase ProveedorDAO con los metodos necesarios para cargar, eliminar, modificar y añadir objetos Proveedor a la Base de Datos

--- src/Modelo/Proveedor.java
+++ src/Modelo/Proveedor.java
@@ -11,56 +11,115 @@ package Modelo;
  */
 public class Proveedor {
 
-    private String NIF;
+    private String nif;
     private String nombre;
     private String direccion;
     private int telefono;
     private String email;
 
-    public Proveedor(String NIF, String nombre, String direccion, int telefono, String email) {
-        this.NIF = NIF;
+    /**
+     *Constructor parametrizado para crear un proveedor con sus atributos
+     * 
+     * @param nif
+     * @param nombre
+     * @param direccion
+     * @param telefono
+     * @param email
+     */
+    public Proveedor(String nif, String nombre, String direccion, int telefono, String email) {
+        this.nif = nif;
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
         this.email = email;
     }
 
-    public String getNIF() {
-        return NIF;
+    /**
+     *Devuelve el nif del proveedor
+     * 
+     * @return
+     */
+    public String getNif() {
+        return nif;
     }
 
-    public void setNIF(String NIF) {
-        this.NIF = NIF;
+    /**
+     *Establece el nif del proveedor
+     * 
+     * @param nif
+     */
+    public void setNif(String nif) {
+        this.nif = nif;
     }
 
+    /**
+     *Devuelve el nombre del proveedor
+     * 
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     *Establece el nombre del proveedor
+     * 
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     *Devuelve la direccion del proveedor
+     * 
+     * @return
+     */
     public String getDireccion() {
         return direccion;
     }
 
+    /**
+     *Establece la direccion del proveedor
+     * 
+     * @param direccion
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     *Devuelve el telefono del proveedor
+     * 
+     * @return
+     */
     public int getTelefono() {
         return telefono;
     }
 
+    /**
+     *Establece el telefono del proveedor
+     * 
+     * @param telefono
+     */
     public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
 
+    /**
+     *Devuelve el email del proveedor
+     * 
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *Establece el email del proveedor
+     * 
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
---
+++ src/Modelo/ProveedorDAO.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Joaquin
+ */
+public class ProveedorDAO {
+
+    private ArrayList<Proveedor> listaProveedores = new ArrayList<>();
+    private Connection con = ConexionBBDD.getConnection();
+
+    /**
+     *Metodo que carga los proveedores desde la base de datros en el atributo
+     * listaProveedores
+     * 
+     */
+    public void cargaProveedorDAO() {
+        listaProveedores.removeAll(listaProveedores);
+        try {
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery("select * from proveedor");
+            String[] datos = new String[5];
+            while (rs.next()) {
+                datos[0] = rs.getString("NIF");
+                datos[1] = rs.getString("Nombre");
+                datos[2] = rs.getString("Direccion");
+                datos[3] = rs.getString("Telefono");
+                datos[4] = rs.getString("Email");
+                Proveedor p1 = new Proveedor(datos[0], datos[1], datos[2], Integer.valueOf(datos[3]), datos[4]);
+                listaProveedores.add(p1);
+            }
+        } catch (Exception e) {
+            System.out.println("Peto al crear el objeto en la bbdd");
+        }
+    }
+
+    /**
+     *Metodo para añadir un nuevo proveedor
+     * 
+     * @param p Objeto de tipo Proveedor
+     */
+    public void anadirProveedor(Proveedor p) {
+        try {
+            Statement stm = con.createStatement();
+            String consulta = "Insert into proveedor "
+                    + "(NIF,Nombre,Direccion,Telefono,Email)"
+                    + "values('" + p.getNif() + "','" + p.getNombre() + "','" + p.getDireccion() + "','" + p.getTelefono() + "','" + p.getEmail() + "')";
+            stm.executeUpdate(consulta);
+            stm.close();
+        } catch (Exception e) {
+            System.out.println("Pero al meter el prov");
+        }
+    }
+
+    /**
+     *Devuelve la lista con todos los objetos almacenados en listaProveedores
+     * @return
+     */
+    public ArrayList<Proveedor> getListaProveedores() {
+        return listaProveedores;
+    }
+
+    /**
+     *Metodo para eliminar un proveedor a partir de su NIF
+     * @param nif
+     */
+    public void eliminarProveedores(String nif) {
+        try {
+            Statement stm = con.createStatement();
+            String consulta = "Delete from proveedor where NIF='" + nif + "'";
+            stm.executeUpdate(consulta);
+            stm.close();
+        } catch (Exception e) {
+            System.out.println("Peto al borrar el prov");
+        }
+    }
+
+    /**
+     *Metodo para modificar un proveedor
+     * 
+     * @param p
+     */
+    public void modificarProveedor(Proveedor p) {
+        try {
+            Statement stm = con.createStatement();
+            String consulta = "update proveedor set nombre='" + p.getNombre() + "',direccion='" + p.getDireccion() + "',telefono=" + p.getTelefono() + ",email='" + p.getEmail() + "'where nif = '" + p.getNif() + "'";
+            stm.executeUpdate(consulta);
+            stm.close();
+        } catch (Exception e) {
+            System.out.println("Peto al modificar el prov");
+        }
+    }
+
+}
